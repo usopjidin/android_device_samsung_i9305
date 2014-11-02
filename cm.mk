a6c412351c7860f74bc7e118cbbcbcfccbb5ab39
usopjidin <usopjidin@gmail.com>
2014-11-02 14:13:56 +0800

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,5 +1,5 @@
 # Release name
-PRODUCT_RELEASE_NAME := i9300
+PRODUCT_RELEASE_NAME := i9305
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
@@ -9,13 +9,13 @@ TARGET_SCREEN_WIDTH := 720
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/i9300/full_i9300.mk)
+$(call inherit-product, device/samsung/i9305/full_i9305.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := i9300
-PRODUCT_NAME := cm_i9300
+PRODUCT_DEVICE := i9305
+PRODUCT_NAME := cm_i9305
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := GT-I9300
+PRODUCT_MODEL := GT-I9305
 PRODUCT_MANUFACTURER := samsung
 
 # Custom unofficial build tag
